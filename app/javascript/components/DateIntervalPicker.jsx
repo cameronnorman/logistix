6eaf0d56382f7060ad3a2d652f05d08c73ae0009
Cameron Norman <cameron.norman@visablelabs.com>
2022-07-31 17:36:45 +0200

    CHORE: JS formatting

--- app/javascript/components/DateIntervalPicker.jsx
+++ app/javascript/components/DateIntervalPicker.jsx
@@ -1,59 +1,75 @@
-import React from 'react'
+import React from "react";
 
-const DateIntervalPicker = ({date, setDate, deliveryInterval, setDeliveryInterval}) => {
-	const possibleDeliveryIntervals = [
-		{
-			key: '15',
-			value: '15'
-		},
-		{
-			key: '30',
-			value: '30'
-		},
-		{
-			key: '45',
-			value: '45'
-		},
-		{
-			key: '60',
-			value: '60'
-		},
-		{
-			key: '75',
-			value: '75'
-		},
-		{
-			key: '90',
-			value: '90'
-		},
-	]
+const DateIntervalPicker = ({
+  date,
+  setDate,
+  deliveryInterval,
+  setDeliveryInterval,
+}) => {
+  const possibleDeliveryIntervals = [
+    {
+      key: "15",
+      value: "15",
+    },
+    {
+      key: "30",
+      value: "30",
+    },
+    {
+      key: "45",
+      value: "45",
+    },
+    {
+      key: "60",
+      value: "60",
+    },
+    {
+      key: "75",
+      value: "75",
+    },
+    {
+      key: "90",
+      value: "90",
+    },
+  ];
 
-	const onDeliveryIntervalChange = (value) => {
-		setDeliveryInterval(value)
-	}
+  const onDeliveryIntervalChange = (value) => {
+    setDeliveryInterval(value);
+  };
 
-	const onDateChange = (value) => {
-		setDate(value)
-	}
+  const onDateChange = (value) => {
+    setDate(value);
+  };
 
-	return <div className="card">
-		<div className="card-body p-4">
-			<div className='field'>
-				<label className="label">Date</label>
-				<div className='control'>
-					<input className="input" type="date" onChange={(e) => onDateChange(e.target.value)} value={date}/>
-				</div>
-			</div>
-			<label className="label">Delivery Interval</label>
-			<div className='select is-fullwidth'>
-				<select className="select" onChange={(e) => onDeliveryIntervalChange(e.target.value)} value={deliveryInterval}>
-					{possibleDeliveryIntervals.map((interval) => {
-						return <option key={interval.key}>{interval.value}</option>
-					})}
-				</select>
-			</div>
-		</div>
-	</div>
-}
+  return (
+    <div className="card">
+      <div className="card-body p-4">
+        <div className="field">
+          <label className="label">Date</label>
+          <div className="control">
+            <input
+              className="input"
+              type="date"
+              onChange={(e) => onDateChange(e.target.value)}
+              value={date}
+            />
+          </div>
+        </div>
+        <label className="label">Delivery Interval</label>
+        <div className="select is-fullwidth">
+          <select
+            className="select"
+            onChange={(e) => onDeliveryIntervalChange(e.target.value)}
+            value={deliveryInterval}
+          >
+            {possibleDeliveryIntervals.map((interval) => {
+              return <option key={interval.key}>{interval.value}</option>;
+            })}
+          </select>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default DateIntervalPicker
+export default DateIntervalPicker;
--- app/javascript/components/Slot.jsx
+++ app/javascript/components/Slot.jsx
@@ -1,38 +1,53 @@
-import React, { useEffect } from 'react'
-import dayjs from 'dayjs';
+import React from "react";
+import dayjs from "dayjs";
 
-const Slot = ({slot, onBookSlotClick}) => {
-	const timeFormat = "HH:mm"
-	const startTime = dayjs(slot.start_date_time)
-	const endTime = dayjs(slot.end_date_time);
-	const startTimeFormatted = startTime.format(timeFormat);
-	const endTimeFormatted = endTime.format(timeFormat);
-	const interval = endTime.diff(startTime, "minute");
-	const cardSize = (interval / 15) ** 2.5
+const Slot = ({ slot, onBookSlotClick }) => {
+  const timeFormat = "HH:mm";
+  const startTime = dayjs(slot.start_date_time);
+  const endTime = dayjs(slot.end_date_time);
+  const startTimeFormatted = startTime.format(timeFormat);
+  const endTimeFormatted = endTime.format(timeFormat);
+  const interval = endTime.diff(startTime, "minute");
+  const cardSize = (interval / 15) ** 2.5;
 
-	const baseClass = `card my-2 mx-1 px-2`
-	const bookedClass = `${baseClass} has-background-danger-light`
+  const baseClass = `card my-2 mx-1 px-2`;
+  const bookedClass = `${baseClass} has-background-danger-light`;
 
-	const cardStyle = {
-		paddingTop: `${cardSize}px`,
-		paddingBottom: `${cardSize}px`
-	}
+  const cardStyle = {
+    paddingTop: `${cardSize}px`,
+    paddingBottom: `${cardSize}px`,
+  };
 
-	useEffect(() => {}, [slot.available])
+  return (
+    <div
+      className={slot.available ? baseClass : bookedClass}
+      key={slot.id}
+      style={cardStyle}
+    >
+      <div className="card-body">
+        <div className="columns">
+          <div className="column">
+            <p className="has-text-weight-bold">
+              {startTimeFormatted} - {endTimeFormatted}
+            </p>
+            <small>{slot.available ? "Available" : "Booked"}</small>
+          </div>
+          <div className="column has-text-right">
+            {slot.available ? (
+              <button
+                className="button is-link"
+                onClick={() => onBookSlotClick(slot)}
+              >
+                Book Slot
+              </button>
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-	return <div className={slot.available ? baseClass : bookedClass} key={slot.id} style={cardStyle}>
-		<div className="card-body">
-			<div className="columns">
-				<div className="column">
-					<p className='has-text-weight-bold'>{startTimeFormatted} - {endTimeFormatted}</p>
-					<small>{slot.available ? "Available" : "Booked"}</small>
-				</div>
-				<div className="column has-text-right">
-					{slot.available ? <button className="button is-link" onClick={() => onBookSlotClick(slot) }>Book Slot</button> : ""}
-				</div>
-			</div>
-		</div>
-	</div>
-}
-
-export default Slot
+export default Slot;
